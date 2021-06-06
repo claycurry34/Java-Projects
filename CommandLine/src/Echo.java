74cd7e0517dffe9406627c7ba713db222ebcb6e0
Clay (Personal) <claycurry34@ou.edu>
2021-06-05 19:31:49 -0500

    backup

---
+++ CommandLine/src/Echo.java
@@ -0,0 +1,9 @@
+public class Echo  {
+	public static void main(String[] args) {
+		for(int i = 0; i < args.length; i++) {
+			// prints each element to command line
+			System.out.print(args[i]);
+		}
+		System.out.println();
+	}
+}
---
+++ Echo/src/Echo.java
@@ -0,0 +1,8 @@
+
+public class Echo {
+// Program writes each String to the standard output
+	public static void main(String[] args) {
+		for(String str : args)
+			System.out.print(str + " ");
+	}
+}
